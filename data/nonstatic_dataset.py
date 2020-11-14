61295c87270d3ce30febd768095ea555ddcb5fcb
Marcos Hirai Catão <hiraimarcos@gmail.com>
2020-11-14 12:35:36 -0300

    Added nonstatic models and dataset

---
+++ data/nonstatic_dataset.py
@@ -0,0 +1,76 @@
+import torch
+import os, re
+import pandas as pd
+import preprocessor as p
+import numpy as np
+from torch.utils.data import Dataset
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+class TweeterDataNonstatic(Dataset):
+    """
+    This is the class for our dataset.
+    When creating an instance of this dataset, select test, val, or train
+    as the setname
+    """
+    def __init__(self, setname, example_length=30):
+        assert setname in ['train1', 'train2', 'train3', 'train4','train', 'test', 'val']
+        self.example_length = example_length
+        self.setname = setname
+        self.path = os.path.join(dir_path, setname)
+        index = os.path.join(self.path, 'index.csv')
+
+        # maps index of points in the dataset to tweet_ids
+        self.index = pd.read_csv(index, index_col=0)
+        self.len = len(self.index)
+
+        # cleans tweet before we can tokenize
+        p.set_options(p.OPT.URL) # remove only URLs
+        self.clean = p.clean
+
+        # patter that identifies all but alphanumeric characters and spaces
+        self.pattern = re.compile(r'([^\s\w]|_)+')
+
+        # get dict that maps word to embeddings
+        self.embeddings = embeddings
+
+    def __len__(self):
+        return len(self.index)
+
+
+    # Returns the tokenized text and label of the entry with the given id
+    def __getitem__(self, id):
+        # get tweet id from index
+        tweet_id = self.index.iloc[id]['Tweet_id']
+        label = int(self.index.iloc[id]['Party']=="D")
+
+        # check whether this tweet was made by republican or democrat
+        # and get the text
+        filename = f"{tweet_id}.txt"
+        if label:
+            path = os.path.join(self.path, "democrat", filename)
+        else:
+            path = os.path.join(self.path, "republican", filename)
+
+        with open(path, "r") as f:
+            text = f.read()
+
+        tweets = self.tokenize(text) # this function will also pad/crop
+        sample = (tweets, label)
+        return sample
+
+    # cleans the tweet and return split version
+    def tokenize(self, text):
+        #  remove urls
+        text = self.clean(text)
+
+        # remove all but alphanumeric and spaces and split tweet
+        text = self.pattern.sub("", text).split()
+
+        # pad or crop so output has length 30
+        if len(text) > self.example_length:
+            text = text[:self.example_length]
+        if len(text) < self.example_length:
+            pad = ['<pad>' for _ in range(self.example_length-len(text))]
+            text += pad
+        return text
---
+++ models/nonstatic.py
@@ -0,0 +1,81 @@
+import torch.nn as nn
+import math, torch
+import preprocessor as p
+
+class CNN_1CONV_MAX(nn.Module):
+    def __init__(self, kernel_size=3, in_length=30, stride, vocab_size=10000, embedding_dim=30):
+        super().__init__()
+        # 1d convolutional layer
+        self.conv = nn.Conv1d(
+            in_channels, 10000,
+            kernel_size=kernel_size,
+            padding=math.floor(kernel_size/2),
+        )
+        self.vocab = dict()
+        self.vocab_max = embedding_dim
+        self.vocab_len = 0 # initialize length to 0
+        p.set_options(p.OPT.URL) # remove only URLs
+        self.clean = p.clean
+        self.pattern = re.compile(r'([^\s\w\@\#])+')
+        self.input_length = in_length
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+
+        conv1_out = math.floor((in_length+2*math.floor(kernel_size/2)-kernel_size) + 1)
+        self.maxpool = nn.MaxPool1d(kernel_size=5)
+        maxpool_out = math.floor(1+(conv1_out-5)/5)
+        self.relu = nn.ReLU()
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(maxpool_out * 10000, 100)
+        self.fc2 = nn.Linear(100, 1)
+
+    def forward(self,x):
+        size = len(x)
+        x = self.tokenize(x)
+        x = self.embedding(x).transpose(0,1)
+        # first convolution
+        x = self.conv(x)
+        x = self.relu(x)
+
+        # maxpool
+        x = self.maxpool(x)
+
+        # flatten output maps
+        x = self.flatten(x)
+
+        # first fully connected layer
+        x = self.fc1(x)
+        x = self.relu(x)
+
+        # second fully connected layer - with binary output
+        x = self.fc2(x)
+
+        # here we use view to make sure the output is a 1d array
+        return x.view(size)
+
+    def tokenize(self, text):
+        # remove anwanted characters and split
+        x = text.lower()
+        x = self.clean(x)
+        x = self.patter.sub("").split()
+        # pad or crop to make sure all examples have same len
+        if len(x) > self.input_length:
+            x = x[:self.input_length]
+        elif len(x) < self.input_length:
+            pad = ['<pad>' for _ in range(self.example_length-len(text))]
+            x = x + pad
+        # initialize list with word indices
+        v = []
+        for word in x:
+            # if word in vocab add word index
+            if word in self.vocab:
+                v.append(self.vocab[word])
+            # if not in vocab but vocab not yet maxed, add to vocab
+            elif self.vocab_len < self.vocab_max:
+                self.vocab[word] = self.vocab_len
+                v.append(vocab_len)
+                self.vocab_len += 1
+            # else, just ignore word by adding <pad> instead
+            else:
+                v.append(self.vocab['<pad>'])
+
+        return torch.Tensor(v).long()
