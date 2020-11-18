448748e7e238b0751d2c89608f03535b90e3dc9b
Marcos Hirai Catão <hiraimarcos@gmail.com>
2020-11-17 22:48:04 -0300

    Fixed output shape

--- models/gru.py
+++ models/gru.py
@@ -20,11 +20,12 @@ class STATIC_GRU(nn.Module):
         """
         Input will be of shape (batch, seq_length, embedding_sie)
         """
+        size = len(x)
         _, x = self.gru(x)
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
 
-        return x
+        return x.view(size)
 
 class NONSTATIC_GRU(nn.Module):
     def __init__(self, embedding_size, hidden_size, batch_first=True, vocab_size=100000):
@@ -38,8 +39,6 @@ class NONSTATIC_GRU(nn.Module):
         batch_first=batch_first
         )
 
-        self.flatten = nn.Flatten()
-
         self.fc1 = nn.Linear(hidden_size, 400)
         self.fc2 = nn.Linear(400, 1)
 
@@ -47,10 +46,10 @@ class NONSTATIC_GRU(nn.Module):
         """
         Input will be of shape (batch, seq_length, embedding_sie)
         """
+        size = len(x)
         x = self.embedding(x)
         _, x = self.gru(x)
-        x = self.flatten(x)
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
 
-        return x
+        return x.view(size)
