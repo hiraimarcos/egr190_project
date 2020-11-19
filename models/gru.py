db7f3f54ca0d827f5dac938e77e71e754519081c
Marcos Hirai Catão <hiraimarcos@gmail.com>
2020-11-19 18:47:45 -0300

    Added new models

--- models/gru.py
+++ models/gru.py
@@ -28,11 +28,12 @@ class STATIC_GRU(nn.Module):
         return x.view(size)
 
 class NONSTATIC_GRU(nn.Module):
-    def __init__(self, embedding_size, hidden_size, batch_first=True, vocab_size=100000):
+    def __init__(self, embedding_size, hidden_size, batch_first=True, vocab_size=100000, dropout=0.25):
         super(NONSTATIC_GRU, self).__init__()
 
         self.embedding = nn.Embedding(vocab_size, embedding_size, padding_idx=0)
         self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout)
 
         self.gru = nn.GRU(
         input_size=embedding_size, hidden_size=hidden_size,
@@ -49,13 +50,15 @@ class NONSTATIC_GRU(nn.Module):
         size = len(x)
         x = self.embedding(x)
         _, x = self.gru(x)
+        x = self.dropout(x)
         x = self.relu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
 
         return x.view(size)
 
 class NONSTATIC_GRU_BIDIRECTIONAL(nn.Module):
-    def __init__(self, embedding_size, hidden_size, batch_first=True, vocab_size=100000):
+    def __init__(self, embedding_size, hidden_size, batch_first=True, vocab_size=100000, dropout=0.25):
         super(NONSTATIC_GRU_BIDIRECTIONAL, self).__init__()
 
         self.embedding = nn.Embedding(vocab_size, embedding_size, padding_idx=0)
@@ -66,6 +69,8 @@ class NONSTATIC_GRU_BIDIRECTIONAL(nn.Module):
         batch_first=batch_first, bidirectional=True
         )
 
+        self.flatten = nn.Flatten()
+        self.dropout = nn.Dropout(dropout)
         self.fc1 = nn.Linear(hidden_size*2, 400)
         self.fc2 = nn.Linear(400, 1)
 
@@ -76,7 +81,10 @@ class NONSTATIC_GRU_BIDIRECTIONAL(nn.Module):
         size = len(x)
         x = self.embedding(x)
         _, x = self.gru(x)
+        x = self.flatten(x)
+        x = self.dropout(x)
         x = self.relu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
 
         return x.view(size)
