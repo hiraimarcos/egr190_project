d6d4eafff22a45b85a1dd0c280fc920d6fa72ef7
Marcos Hirai Catão <hiraimarcos@gmail.com>
2020-11-19 18:36:31 -0300

    Added new models

--- models/gru.py
+++ models/gru.py
@@ -53,3 +53,30 @@ class NONSTATIC_GRU(nn.Module):
         x = self.fc2(x)
 
         return x.view(size)
+
+class NONSTATIC_GRU_BIDIRECTIONAL(nn.Module):
+    def __init__(self, embedding_size, hidden_size, batch_first=True, vocab_size=100000):
+        super(NONSTATIC_GRU, self).__init__()
+
+        self.embedding = nn.Embedding(vocab_size, embedding_size, padding_idx=0)
+        self.relu = nn.ReLU()
+
+        self.gru = nn.GRU(
+        input_size=embedding_size, hidden_size=hidden_size,
+        batch_first=batch_first, bidirectional=True
+        )
+
+        self.fc1 = nn.Linear(hidden_size*2, 400)
+        self.fc2 = nn.Linear(400, 1)
+
+    def forward(self, x):
+        """
+        Input will be of shape (batch, seq_length, embedding_sie)
+        """
+        size = len(x)
+        x = self.embedding(x)
+        _, x = self.gru(x)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x.view(size)
--- models/nonstatic.py
+++ models/nonstatic.py
@@ -121,6 +121,83 @@ class CNN_3CONV_MAX_NONSTATIC(nn.Module):
 
         return x.view(size)
 
+class CNN_4CONV_MAX_NONSTATIC_v2(nn.Module):
+    def __init__(self, in_length=30, vocab_size=100000, embedding_dim=64, dropout=0.25):
+        super(CNN_3CONV_MAX_NONSTATIC, self).__init__()
+        self.relu = nn.ReLU()
+        self.flatten = nn.Flatten()
+        self.dropout = nn.Dropout(dropout)
+
+        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
+
+        self.conv1 = nn.Conv1d(
+            embedding_dim, 50,
+            kernel_size = 3,
+            padding = 1,
+        )
+        self.maxpool1 = nn.MaxPool1d(2, stride=2)
+
+        self.conv2 = nn.Conv1d(
+            50, 200,
+            kernel_size=3,
+            stride=1,
+            padding=1
+        )
+        self.maxpool2 = nn.MaxPool1d(2, stride=2, padding=1)
+        self.conv3 = nn.Conv1d(
+            200, 400,
+            kernel_size=2,
+            stride=1,
+            padding=1
+        )
+        self.maxpool3 = nn.MaxPool1d(2, stride=2)
+        self.conv4 = nn.Conv1d(
+            400, 400,
+            kernel_size=2,
+            stride=1,
+            padding=1
+        )
+        self.maxpool4 = nn.MaxPool1d(2, stride=2)
+        self.fc1 = nn.Linear(800, 200)
+        self.fc2 = nn.Linear(200, 1)
+
+    def forward(self, x):
+        size = len(x)
+
+        x = self.embedding(x).transpose(1,2)
+
+        # first convolutional layer
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool1(x)
+
+        # second convolutional layer
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool2(x)
+
+        # third convolutional layer
+        x = self.conv3(x)
+        x = self.relu(x)
+        x = self.maxpool3(x)
+
+        # fourth convolutional layer
+        x = self.conv3(x)
+        x = self.relu(x)
+        x = self.maxpool3(x)
+
+        # flatten feature maps
+        x = self.flatten(x)
+        x = self.dropout(x)
+        # first fully connected layer
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        # second fully connected layer
+        x = self.fc2(x)
+
+        return x.view(size)
+
 class CNN_4CONV_MAX_NONSTATIC(nn.Module):
     def __init__(self, in_length=30, vocab_size=100000, embedding_dim=64, dropout=0.25):
         super(CNN_4CONV_MAX_NONSTATIC, self).__init__()
